0c1e8638a01db3090cc5313a76ae519a1ac02688
ashishpatel8583-ctrl <ashishpatel8583@gmail.com>
2025-11-27 21:04:51 +0530

    Update mongo.py

--- database/mongo.py
+++ database/mongo.py
@@ -1,1 +1,9 @@
+from motor.motor_asyncio import AsyncIOMotorClient
 
+import os
+
+MONGO_URI = os.getenv("MONGO_URI")
+client = AsyncIOMotorClient(MONGO_URI)
+
+db = client.telegram_bot   # database name
+tasks = db.tasks           # collection name
