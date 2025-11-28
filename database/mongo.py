b5250f86841711f6185ea7f22e5dcb2c87e3d4df
ashishpatel8583-ctrl <ashishpatel8583@gmail.com>
2025-11-28 08:55:01 +0530

    Change MongoDB URI and database name
    Updated MongoDB connection URI and database name.

--- database/mongo.py
+++ database/mongo.py
@@ -2,8 +2,8 @@ from motor.motor_asyncio import AsyncIOMotorClient
 
 import os
 
-MONGO_URI = os.getenv("MONGO_URI")
+MONGO_URI = os.getenv("mongodb+srv://bobbyutube48_db_user:er9RNl8mXVoJ3zlt@cluster0.akhtmm9.mongodb.net/?appName=Cluster0")
 client = AsyncIOMotorClient(MONGO_URI)
 
-db = client.telegram_bot   # database name
+db = client.Cluster0   # database name
 tasks = db.tasks           # collection name
